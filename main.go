b565df8c9bc907abb5d0a4994961ac362235277a
Irabeny <irabeny89@gmail.com>
2026-04-20 23:36:53 +0100

    refactor: replace fmt print messages with custom logger

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -22,15 +21,15 @@ func main() {
 		logger.Warn("PORT env var not found", "PORT", p)
 	}
 
-	// 1. Create a channel to listen for OS signals
+	// Create a channel to listen for OS signals
 	// We use a buffer of 1 so we don't miss the signal
 	sigChan := make(chan os.Signal, 1)
 
-	// 2. Register the signals we want to capture
+	// Register the signals we want to capture
 	// SIGINT = Ctrl+C, SIGTERM = Standard termination (Docker/Linux)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
-	// 3. Run your app logic in a separate goroutine if needed,
+	// Run your app logic in a separate goroutine if needed,
 	// or just wait for the signal here.
 	go func() {
 		logger.Info("Server is running...")
@@ -40,14 +39,14 @@ func main() {
 		}
 	}()
 
-	// 4. Block here until a signal is received
+	//! Block here until a signal is received
 	sig := <-sigChan
-	fmt.Printf("\nReceived signal: %v. Shutting down...\n", sig)
+	logger.Info("Shutting down...", "signal", sig)
 
-	// 5. Trigger your graceful shutdown with the timeout logic
+	// Trigger your graceful shutdown with the timeout logic
 	if err := client.Close(); err != nil {
-		fmt.Printf("Error during shutdown: %v\n", err)
+		logger.Error("Error during shutdown", "err", err)
 	}
 
-	fmt.Println("Goodbye!")
+	logger.Info("Goodbye!")
 }
